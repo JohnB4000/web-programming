e4cbe52d49b1f7aa174334aaa8c4e211288ca1b8
JohnB4000 <jb2069@hw.ac.uk>
2023-10-05 16:47:25 +0100

    Formatting

--- week-3/week3script.js
+++ week-3/week3script.js
@@ -1,17 +1,17 @@
 function checkData() {
 	var name = document.getElementById("user").value;
 	if ( name.length < 2 || (/[^A-Za-z\s\-]/).test(name) ) {
-	  alert("Name should have 2 or more letters, hyphens, spaces");
-	  return false;
+		alert("Name should have 2 or more letters, hyphens, spaces");
+		return false;
 	}
 	var age = document.getElementById("age").value;
 	if ( age.length == 0 || (/[^0-9]/).test(age) ||
-	  age < 0 || age > 120 ) {
-	  alert("Please give age between 0 and 120");
-	  return false;
+		age < 0 || age > 120 ) {
+		alert("Please give age between 0 and 120");
+		return false;
 	}
 	return true;
-  }
+}
 
 
 var map = L.map('map').setView([55.93, -3.30], 12);
