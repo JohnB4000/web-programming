6b1defd2dbe857f9937b7ea327787e796bb80f10
JohnB4000 <jb2069@hw.ac.uk>
2023-10-09 10:19:46 +0100

    Add second RSS feed

--- week-5/index.js
+++ week-5/index.js
@@ -1,25 +1,17 @@
-async function fetchData() {
+async function fetchData(url) {
 	try {
-		const response = await fetch('https://moxie.foxnews.com/google-publisher/latest.xml');
+		const response = await fetch(url);
 		const text = await response.text();
-		console.log(text.substring(0, 100));
 
 		let parser = new DOMParser();
 		let xmlDoc = parser.parseFromString(text, "text/xml");
 
-		console.log( xmlDoc );
-
 		const items = xmlDoc.querySelectorAll("item");
-		console.log('items:', items );
-		
+
 		items.forEach( (el)=>{
 			let title       = el.querySelector('title').textContent;
 			let description = el.querySelector('description').textContent;
 			let guid        = el.querySelector('guid').textContent;
-		
-			console.log('title:',       title       );
-			console.log('description:', description);
-			console.log('guid:',        guid);
 		
 			let div = document.createElement('div');
 			document.body.appendChild( div );
@@ -36,4 +28,7 @@ async function fetchData() {
 	}
 }
 
-fetchData();
+const foxUrl = 'https://moxie.foxnews.com/google-publisher/latest.xml';
+const newYorkUrl = 'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml';
+fetchData(foxUrl);
+fetchData(newYorkUrl);
