8267f603500e430d63cd942d8845873f384c5a61
JohnB4000 <jb2069@hw.ac.uk>
2023-11-18 14:08:18 +0000

    Fix formatting

--- week-10/index.js
+++ week-10/index.js
@@ -1,28 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-var path = require('path')
+var path = require("path");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
 });
 
 var toDoId = 0;
 
-io.on('connection', (socket) => {
-	socket.on('new-to-do', (msg) => {
-		io.emit('add-new-to-do', msg, toDoId++);
+io.on("connection", (socket) => {
+	socket.on("new-to-do", (msg) => {
+		io.emit("add-new-to-do", msg, toDoId++);
 	});
-	socket.on('delete-to-do', (id) => {
-		io.emit('delete-to-do-item', id);
+	socket.on("delete-to-do", (id) => {
+		io.emit("delete-to-do-item", id);
 	});
 });
 
 server.listen(3000, () => {
-	console.log('listening on *:3000');
+	console.log("listening on *:3000");
 });
