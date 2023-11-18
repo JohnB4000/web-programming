ab94a98610f182482d6f31ac86345945daaaca9a
JohnB4000 <jb2069@hw.ac.uk>
2023-11-18 14:33:12 +0000

    Update check socket and add array for storage

--- week-10/index.js
+++ week-10/index.js
@@ -6,23 +6,46 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 var path = require("path");
 
+var toDoId = 0;
+var todos = []
+
+function deleteToDo(id) {
+	for (let i = 0; i < todos.length; i++) {
+		if (todos[i].id === id) {
+			todos.splice(i, 1);
+			break;
+		}
+	}
+}
+
+function updateState(id, state) {
+	for (let i = 0; i < todos.length; i++) {
+		if (todos[i].id === id) {
+			todos[i].state = state;
+			break;
+		}
+	}
+}
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-var toDoId = 0;
 
 io.on("connection", (socket) => {
 	socket.on("new-to-do", (msg) => {
-		io.emit("add-new-to-do", msg, toDoId++);
+		io.emit("add-new-to-do", msg, toDoId);
+		todos.push({ id: toDoId++, name: msg, state: false })
 	});
 	socket.on("delete-to-do", (id) => {
 		io.emit("delete-to-do-item", id);
+		deleteToDo(id)
 	});
-	socket.on("check-to-do", (data) => {
-		io.emit("check-to-do-item", data);
+	socket.on("check-to-do", (id, state) => {
+		io.emit("check-to-do-item", id, state);
+		updateState(id, state)
 	});
 });
 
