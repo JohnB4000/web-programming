5b32ea1d7ba93ecf82765659ba646dda518da331
JohnB4000 <jb2069@hw.ac.uk>
2023-11-22 14:33:52 +0000

    Initial commit for week 11 and fix checkbox size

---
+++ week-11/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var path = require("path");
+
+var toDoId = 0;
+var todos = []
+
+function deleteToDo(id) {
+	for (let i = 0; i < todos.length; i++) {
+		if (todos[i].id === id) {
+			todos.splice(i, 1);
+			break;
+		}
+	}
+}
+
+function updateState(id, state) {
+	for (let i = 0; i < todos.length; i++) {
+		if (todos[i].id === id) {
+			todos[i].state = state;
+			break;
+		}
+	}
+}
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+
+io.on("connection", (socket) => {
+	for (let i = 0; i < todos.length; i++) {
+		io.emit("add-new-to-do", todos[i].id, todos[i].name, todos[i].state)
+	}
+
+	socket.on("new-to-do", (msg, state) => {
+		io.emit("add-new-to-do", toDoId, msg, state);
+		todos.push({ id: toDoId++, name: msg, state: false })
+	});
+	socket.on("delete-to-do", (id) => {
+		io.emit("delete-to-do-item", id);
+		deleteToDo(id)
+	});
+	socket.on("check-to-do", (id, state) => {
+		io.emit("check-to-do-item", id, state);
+		updateState(id, state)
+	});
+});
+
+server.listen(3000, () => {
+	console.log("listening on *:3000");
+});
