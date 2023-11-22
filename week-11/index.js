5f588fc74357c5ebfbba4bd28c65dcf59c3e33e8
JohnB4000 <jb2069@hw.ac.uk>
2023-11-22 15:09:20 +0000

    Fix problem with not passing data correctly

--- week-11/index.js
+++ week-11/index.js
@@ -57,10 +57,10 @@ function deleteToDo(data, id) {
 	}
 }
 
-function updateState(id, state) {
-	for (let i = 0; i < todos.length; i++) {
-		if (todos[i].id === id) {
-			todos[i].state = state;
+function updateState(data, id, state) {
+	for (let i = 0; i < data.length; i++) {
+		if (data[i].id === id) {
+			data[i].state = state;
 			break;
 		}
 	}
@@ -78,7 +78,7 @@ io.on("connection", (socket) => {
 	});
 
 	socket.on("new-to-do", (msg, state) => {
-		let newToDo = { id: toDoCount++, name: msg, state: false }
+		let newToDo = { id: toDoCount++, name: msg, state: state }
 		io.emit("add-new-to-do", newToDo);
 		let todos = readToDos();
 		todos.push(newToDo);
@@ -86,15 +86,15 @@ io.on("connection", (socket) => {
 	});
 	socket.on("delete-to-do", (id) => {
 		io.emit("delete-to-do-item", id);
-		let todos = readToDos();
-		deleteToDo(id);
-		writeToDos(todos);
+		let toDos = readToDos();
+		deleteToDo(toDos, id);
+		writeToDos(toDos);
 	});
 	socket.on("check-to-do", (id, state) => {
 		io.emit("check-to-do-item", id, state);
-		let todos = readToDos();
-		updateState(id, state);
-		writeToDos(todos);
+		let toDos = readToDos();
+		updateState(toDos, id, state);
+		writeToDos(toDos);
 	});
 });
 
